620cf42671a3c9f54a9ab693b02c134dc4c4ca98
Suryansh Bhatnagar <sbh2068@gmail.com>
2024-04-21 22:02:36 +0530

    cors error fixed

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.CLIENT_URL,
         credentials: true,
     })
 )
